2f3f14a9c6787dda985b70545d58ebd5a9d47a35
Vincenzo Pierro <vcp@icure.com>
2024-12-10 15:25:50 +0100

    Upgraded to PaginatedFlow

--- standalone/src/main/kotlin/org/taktik/icure/config/WebConfig.kt
+++ standalone/src/main/kotlin/org/taktik/icure/config/WebConfig.kt
@@ -7,45 +7,48 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
-import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.taktik.icure.spring.encoder.FluxStringJsonEncoder
+import org.taktik.icure.spring.encoder.PaginatedCollectingJackson2JsonEncoder
 
 @Configuration
 @EnableWebFlux
 class WebConfig : WebFluxConfigurer {
 
-    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
-        configurer.defaultCodecs().maxInMemorySize(128 * 1024 * 1024)
+	override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+		configurer.defaultCodecs().maxInMemorySize(128 * 1024 * 1024)
 
-        configurer.defaultCodecs().jackson2JsonEncoder(
-            Jackson2JsonEncoder(
-                ObjectMapper().registerModule(
-                    KotlinModule.Builder()
-                        .configure(KotlinFeature.NullIsSameAsDefault, true)
-                        .build()
-                ).apply { setSerializationInclusion(JsonInclude.Include.NON_NULL) }
-            )
-        )
-        configurer.defaultCodecs().jackson2JsonDecoder(
-            Jackson2JsonDecoder(
-                ObjectMapper().registerModule(
-                    KotlinModule.Builder()
-                        .configure(KotlinFeature.NullIsSameAsDefault, true)
-                        .build()
-                )
-            ).apply { maxInMemorySize = 128 * 1024 * 1024 }
-        )
-    }
+		configurer.customCodecs().register(FluxStringJsonEncoder())
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry
-            .addMapping("/**")
-            .allowCredentials(true)
-            .allowedOriginPatterns("*")
-            .allowedMethods("*")
-            .allowedHeaders("*")
-    }
+		configurer.defaultCodecs().jackson2JsonEncoder(
+			PaginatedCollectingJackson2JsonEncoder(
+				ObjectMapper().registerModule(
+					KotlinModule.Builder()
+						.configure(KotlinFeature.NullIsSameAsDefault, true)
+						.build()
+				).apply { setSerializationInclusion(JsonInclude.Include.NON_NULL) }
+			)
+		)
+		configurer.defaultCodecs().jackson2JsonDecoder(
+			Jackson2JsonDecoder(
+				ObjectMapper().registerModule(
+					KotlinModule.Builder()
+						.configure(KotlinFeature.NullIsSameAsDefault, true)
+						.build()
+				)
+			).apply { maxInMemorySize = 128 * 1024 * 1024 }
+		)
+	}
+
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry
+			.addMapping("/**")
+			.allowCredentials(true)
+			.allowedOriginPatterns("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+	}
 
 }
