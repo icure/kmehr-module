e25064823555e9a69812d01c9a45e46d6e031633
Clément Vandendaelen <vandendaelenclement@gmail.com>
2026-05-08 18:21:19 +0200

    Updated kraken-common submodule (#334)
    * Updated kraken-common 202605081311-ec1e5ed701294fd13e4b0130274d60b336d7895e
    
    * Tested with user with minimal permissions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Vincenzo Pierro <vcp@icure.com>

--- standalone/src/test/kotlin/org/taktik/icure/test/EnvironmentBootstrapper.kt
+++ standalone/src/test/kotlin/org/taktik/icure/test/EnvironmentBootstrapper.kt
@@ -1,11 +1,20 @@
 package org.taktik.icure.test
 
+import com.icure.cardinal.sdk.api.raw.RawApiConfig
+import com.icure.cardinal.sdk.api.raw.impl.RawUserApiImpl
+import com.icure.cardinal.sdk.model.ListOfIds
+import com.icure.cardinal.sdk.model.User
+import com.icure.cardinal.sdk.options.RequestRetryConfiguration
+import com.icure.cardinal.sdk.utils.Serialization
 import com.icure.test.setup.ICureTestSetup
+import com.icure.utils.InternalIcureApi
 import io.kotest.common.runBlocking
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.expectSuccess
 import io.ktor.client.request.basicAuth
+import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
@@ -13,12 +22,15 @@ import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.taktik.icure.entities.UserType
 import java.io.File
+import kotlin.time.ExperimentalTime
 
 class EnvironmentBootstrapper : ApplicationContextInitializer<ConfigurableApplicationContext> {
 
 	private val composeDir = "src/test/resources/docker"
 	private val krakenCompose = System.getenv("KRAKEN_COMPOSE") ?: "file://$composeDir/docker-compose-cloud.yaml"
+	private val masterUserId = "f422c984-4af8-11f1-8ec8-f78509c56b89"
 
+	@OptIn(InternalIcureApi::class, ExperimentalTime::class)
 	override fun initialize(applicationContext: ConfigurableApplicationContext) {
 		runBlocking {
 			ICureTestSetup.startKrakenEnvironment(krakenCompose, emptyList(), composeDir)
@@ -32,6 +44,58 @@ class EnvironmentBootstrapper : ApplicationContextInitializer<ConfigurableApplic
 				rootUserRoles = defaultRoles
 			)
 			loadRolesInConfig()
+			createMasterUser()
+		}
+	}
+
+	private suspend fun createMasterUser() {
+		val isUserMissing = testHttpClient.get("http://localhost:15984/icure-xx-base/$masterUserId") {
+			basicAuth("icure", "icure")
+			contentType(ContentType.Application.Json)
+		}.status.value == 404
+		if (isUserMissing) {
+			val user = """
+			{
+			  "_id": "$masterUserId",
+			  "login": "jack",
+			  "passwordHash": "1796980233375ccd113c972d946b2c4a7892e4f69c60684cfa730150047f9c0b",
+			  "isUse2fa": true,
+			  "type": "database",
+			  "status": "ACTIVE",
+			  "java_type": "org.taktik.icure.entities.User"
+			}
+		""".trimIndent()
+			testHttpClient.post("http://localhost:15984/icure-xx-base") {
+				basicAuth("icure", "icure")
+				contentType(ContentType.Application.Json)
+				setBody(user)
+				expectSuccess = true
+			}
+			val userOnBase = """
+			{
+			  "_id": "xx:$masterUserId",
+			  "identifier": [],
+			  "loginIdentifiers": [],
+			  "hashedLoginIdentifiers": [],
+			  "status": "ACTIVE",
+			  "passwordHash": "1796980233375ccd113c972d946b2c4a7892e4f69c60684cfa730150047f9c0b",
+			  "userType": "USER",
+			  "login": "jack",
+			  "loginHash": "MWERWefm/3hD6kYndF6JIl/IZmIc/P29QIca9EE3R8w=",
+			  "groupId": "xx",
+			  "authenticationTokens": {},
+			  "permissions": [],
+			  "roles": ["xx:KMEHR_ROLE"],
+			  "inheritsPermissionsToSubgroups": true,
+			  "java_type": "org.taktik.icure.entities.User"
+			}
+		""".trimIndent()
+			testHttpClient.post("http://localhost:15984/icure-__-base") {
+				basicAuth("icure", "icure")
+				contentType(ContentType.Application.Json)
+				setBody(userOnBase)
+				expectSuccess = true
+			}
 		}
 	}
 
